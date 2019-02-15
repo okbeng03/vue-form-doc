7963accbae90c49db26bb0227ffede6f0851d761
chb.wang <chb.wang@hnair.com>
2019-02-15 10:31:54 +0800

    Update docs

---
+++ examples/fieldset.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import _ from 'lodash'
+import VueForm from 'jsonschema-form-vue'
+import { store } from 'jsonschema-form-vue'
+import App from './fieldset.vue'
+
+Vue.config.productionTip = false
+Vue.use(Vuex)
+Vue.use(VueForm)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store: new Vuex.Store(store),
+  components: { App },
+  template: '<App ref="app"/>'
+})
